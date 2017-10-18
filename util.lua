3dcacc061c2be127fc145e2e195beaff37b8b638
Hydroque <deocojo1@gmail.com>
2017-10-18 11:28:45 -0400

    Removed useless parameter

--- util.lua
+++ util.lua
@@ -18,25 +18,25 @@ util.build_args = function(arr)
 	return string.sub(out, 1, #out-1)
 end
 
-util.encode_url = function(url, protocol)
+util.encode_url = function(url)
 	local out = ""
 	for i=1, #url do
-		local char = url:sub(i,i)
-		local byte = string.byte(char)
-		local ch = string.gsub(char, "^[%c\"<>#%%%s{}|\\%^~%[%]`]+", function(s)
-			return string.format("%%%02x", byte)
+		local ch = url:sub(i,i)
+		local by = string.byte(ch)
+		local ch = string.gsub(ch, "^[%c\"<>#%%%s{}|\\%^~%[%]`]+", function(s)
+			return string.format("%%%02x", by)
 		end)
-		if(byte > 126)then
-			ch = string.format("%%%02x", byte)
+		if(by > 126)then
+			ch = string.format("%%%02x", by)
 		end
 		out = out .. ch
 	end
-	return (protocol or "") .. out
+	return out
 end
 
 util.build_uri = function(secret, name, initial_count, issuer_name, algorithm, digits, period)
 	local label = util.encode_url(name)
-	label = issuer_name and util.encode_url(issuer_name) .. ':' .. label or ""
+	label = issuer_name and (util.encode_url(issuer_name) .. ':' .. label) or ""
 	
 	algorithm = algorithm and string.upper(algorithm) or ""
 	
@@ -49,15 +49,6 @@ util.build_uri = function(secret, name, initial_count, issuer_name, algorithm, d
 		period = period
 	}
 	return string.format(util.base_uri, initial_count ~= nil and "hotp" or "totp", label, util.build_args(url_args))
-end
-
-util.strings_equal = function(s1, s2)
-	for i=1, #s1 do
-		if(s1:sub(i,i) ~= s2:sub(i,i))then
-			return false
-		end
-	end
-	return true
 end
 
 util.arr_reverse = function(tab)
